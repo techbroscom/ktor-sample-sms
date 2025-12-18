5a6207c666a2d5b7233540ccd962612da347464c
manisankarsms <manisankarsms@gmail.com>
2025-12-18 14:51:04 +0530

    Jobs for otp and fcm

--- src/main/kotlin/plugins/Jobs.kt
+++ src/main/kotlin/plugins/Jobs.kt
@@ -11,7 +11,7 @@ import io.ktor.server.application.*
 
 fun Application.configureJobs() {
 
-    /*val userRepository = UserRepository()
+    val userRepository = UserRepository()
     val otpRepository = OtpRepository()
     val emailService = EmailService()
     val otpService = OtpService(otpRepository, userRepository, emailService)
@@ -27,5 +27,5 @@ fun Application.configureJobs() {
     environment.monitor.subscribe(ApplicationStopped) {
         otpCleanupJob.stop()
         fcmCleanupJob.stop()
-    }*/
+    }
 }
