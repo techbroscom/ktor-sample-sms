75daacc9419cb1c251f6fd8c6cc55547769035a3
manisankarsms <manisankarsms@gmail.com>
2025-12-20 16:46:07 +0530

    fix

--- src/main/kotlin/models/dto/StaffPermissionDto.kt
+++ src/main/kotlin/models/dto/StaffPermissionDto.kt
@@ -1,5 +1,8 @@
 package com.example.models.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class StaffPermissionDto(
     val user: UserDto,
     val permissions: List<UserPermissionDto>
